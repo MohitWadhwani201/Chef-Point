5769b4d17cdc6a8a0a1abb07230993af8af24f72
MohitWadhwaniWebseeder <wadhwani.mohit@webseeder.tech>
2025-09-25 04:12:44 +0530

    changes css

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,22 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar({ user, logout }) {
-  return (
-    <nav className="navbar">
-      <div className="navbar-brand">Recipe Generator</div>
-      <ul className="navbar-nav">
-        <li><Link to="/">Generate Recipe</Link></li>
-        <li><Link to="/my-recipes">My Recipes</Link></li>
-        <li>
-          <span>Welcome, {user.username}</span>
-          <button onClick={logout} style={{ marginLeft: '1rem' }} className="btn btn-secondary">
-            Logout
-          </button>
-        </li>
-      </ul>
-    </nav>
-  );
+	return (
+		<nav className="navbar">
+			<div className="navbar-brand">Recipe Generator</div>
+			<ul className="navbar-nav">
+				<li>
+					<Link to="/" className="navbar-mycomponent">
+						Generate Recipe
+					</Link>
+				</li>
+				<li>
+					<Link to="/my-recipes" className="navbar-mycomponent">
+						My Recipes
+					</Link>
+				</li>
+				<li>
+					<span>Welcome, {user.username}</span>
+					<button onClick={logout} style={{ marginLeft: "1rem" }} className="btn btn-secondary">
+						Logout
+					</button>
+				</li>
+			</ul>
+		</nav>
+	);
 }
 
 export default Navbar;
